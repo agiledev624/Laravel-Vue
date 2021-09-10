c85c1ec392598e40fe8108bd462445e52c91103e
agiledev624 <agiledev624@gmail.com>
2021-09-10 16:16:53 +0900

    Revert "Created form"
    This reverts commit bd977403796317cb161a09b5b864732a9183cd14.

--- routes/web.php
+++ routes/web.php
@@ -25,13 +25,17 @@ Route::get('/{any}', 'CompaniesController@index')->where('any', '.*');
 
 Route::get('/{any}', 'CompaniesController@index')->where('any', '.*');
 
+<<<<<<< HEAD
 >>>>>>> parent of bd97740 (Created form)
+=======
+>>>>>>> parent of bd97740... Created form
 // Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin_dashboard', 'Admin\DashboardController@index')->middleware('role:admin');
 Route::get('/seller_dashboard', 'Seller\DashboardController@index')->middleware('role:seller');
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
@@ -52,3 +56,8 @@ Route::get('/{any}', 'CompaniesController@index')->where('any', '.*');
 //     Route::get('companies', 'CompaniesController@index')->name('companies.index');
 // });
 >>>>>>> parent of bd97740 (Created form)
+=======
+// Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+//     Route::get('companies', 'CompaniesController@index')->name('companies.index');
+// });
+>>>>>>> parent of bd97740... Created form
