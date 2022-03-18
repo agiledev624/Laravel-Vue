1a8f97ad9b65d5efb2a1cf40023608d356841dcc
agiledev624 <agiledev624@gmail.com>
2022-03-18 17:09:14 +0800

    backend fix again

--- resources/assets/js/components/companies/Status.vue
+++ resources/assets/js/components/companies/Status.vue
@@ -161,7 +161,10 @@ export default {
       if (confirm("Do you really want to open it?")) {
         var app = this;
         axios
-          .post("/api/v1/lockers/open_locker", { port: $userPort, number: id })
+          .post("/api/v1/lockers/open_locker", {
+            port: this.$userPort,
+            number: id,
+          })
           .then(function (resp) {
             if (resp.data.result != 0) return;
             var app = this;
